68e8717078727d4b2053d5a5f4051ff1c394fb40
usrhans <116822779+usrhans@users.noreply.github.com>
2022-10-27 20:31:20 +0530

    Update prime-numbers.js

--- javascript/prime-numbers.js
+++ javascript/prime-numbers.js
@@ -1,6 +1,21 @@
+// Method 1
 function isPrime(num) {
     for (let i = 2; i * i <= num; i++)
         if (num % i === 0)
           return false; 
     return num > 1;
+}
+
+// Method 2
+function getPrime(until) {
+  let factor;
+  for (let i = 0; i < n; i++) {
+    factor = 0;
+    for (let j = 1; j <= n; j++) {
+      if (i % j == 0) {
+        factor++;
+      }
+    }
+    if (factor == 2) console.log(i);
+  }
 }
